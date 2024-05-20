65fdfd739ee6ff6504c6ba94e556c4fd65d9e3b6
Coroutine <hung.l.g.ngo@gmail.com>
2024-05-20 21:31:03 +0700

    Add some interface.

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/springframework/services/VetService.java
@@ -0,0 +1,14 @@
+package com.springframework.services;
+
+import com.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
