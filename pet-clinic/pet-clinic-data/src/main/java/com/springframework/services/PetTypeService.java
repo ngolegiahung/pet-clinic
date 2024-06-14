f3ec59052afb12db2324fc29cbfa797db6b79fa4
Coroutine <hung.l.g.ngo@gmail.com>
2024-06-14 22:28:37 +0700

    Add PetTypeService.

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/springframework/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.services;
+
+import com.springframework.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/com/springframework/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.springframework.services.map;
+
+import com.springframework.model.PetType;
+import com.springframework.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+}
