4681079c9241adec6b6ea5af677df861afd3252c
Coroutine <hung.l.g.ngo@gmail.com>
2024-06-25 21:52:43 +0700

    Basic repository.

--- pet-clinic/pet-clinic-data/src/main/java/com/springframework/repository/OwnerRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/springframework/repository/OwnerRepository.java
@@ -2,6 +2,9 @@ package com.springframework.repository;
 
 import com.springframework.model.Owner;
 import com.springframework.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudService<Owner, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
