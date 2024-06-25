6c77b4459c23a52af29aae160bb47c4e7c467c67
Coroutine <hung.l.g.ngo@gmail.com>
2024-06-25 20:53:46 +0700

    JPA - Owner, Pet, PetType.

--- pet-clinic/pet-clinic-data/src/main/java/com/springframework/model/Owner.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/springframework/model/Owner.java
@@ -1,22 +1,25 @@
 package com.springframework.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
-    private Set<Pet> pets = new HashSet<>();
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
@@ -40,5 +43,13 @@ public class Owner extends Person {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
